4d3cbecb157d7abc90a8f7f15371782b31fba586
yuganshghorpade <yuganshghorpade2005@gmail.com>
2024-09-05 12:37:49 +0530

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our documents
           </a>
         </div>
       </main>
